895b5409e9385f837555b513afb249efa7b84936
github username <github email address>
2019-07-27 13:21:48 -0400

    Meet minimum requirements of lab - no bonus features implemented.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    const input = document.getElementById("new-task-description").value // grab input
+    const li = document.createElement("LI") // create li
+    const text = document.createTextNode(input) // set up text and button
+    const button = document.createElement("button")
+    button.innerHTML = "X"
+
+    li.appendChild(text) // add text and button to li
+    li.appendChild(button)
+    document.getElementById("tasks").appendChild(li); // append li
+    document.getElementById("create-task-form").reset() // clear form
+  });
 });
