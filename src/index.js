87efeffb479ca7ff49022774a65273cd285fcef2
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-08-12 20:42:43 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('#create-task-form').addEventListener('submit', handleFormSubmit);
 });
+
+const userInputValue = () => {
+  return document.querySelector('#new-task-description').value
+}
+
+const handleFormSubmit = event => {
+  event.preventDefault();
+  addNewLI();
+}
+
+const addNewLI = () => {
+  const taskList = document.querySelector('ul#tasks')
+  const newLI = document.createElement('li')
+  newLI.innerText = userInputValue();
+  taskList.append(newLI);
+}
