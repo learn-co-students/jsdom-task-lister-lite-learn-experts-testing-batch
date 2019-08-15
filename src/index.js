d25c55cea44c9e63db2ca53a68e21d356d44f895
austin_melchior <raustin.melchior@gmail.com>
2019-08-15 13:02:37 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector("#create-task-form")
+
+  const addFormListener = e => {
+    e.preventDefault();
+    let taskList = document.querySelector("#tasks")
+    let item = document.createElement("li");
+    item.id = guidGenerator()
+    let newListItem = document.querySelector("#new-task-description")
+    item.innerHTML +=  `${newListItem.value} <button onClick=handleClick("${item.id}")>X</button>`
+    taskList.prepend(item)
+    newListItem.value = ""
+  }
+
+  form.addEventListener("submit", addFormListener);
+
+})
+
+function handleClick(id) {
+  let listItem = document.getElementById(id);
+  listItem.remove()
+}
+
+function guidGenerator() {
+  var S4 = function() {
+     return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+  };
+  return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
